d315654fef5df5ea10ba67b7397c1de890e8feb4
bozanbungee <bozhan.georgiev@gmail.com>
2023-12-14 12:53:24 +0200

    End of lesson 6

---
+++ resources/js/Pages/Admin/Users/UserIndex.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="User" />
+    
+
+    <AdminLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">User</h2>
+        </template>
+        
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    
+                    <h1 class="text-3xl text-green-400">Hello. This is User Index Page</h1>
+                </div>
+            </div>
+        </div>
+    </AdminLayout>
+</template>
