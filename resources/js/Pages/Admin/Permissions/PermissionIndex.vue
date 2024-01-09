e8401370681c781b3cbb898f2095181e8cf4bb44
bozanbungee <bozhan.georgiev@gmail.com>
2024-01-09 14:44:07 +0200

    End of lesson 11

--- resources/js/Pages/Admin/Permissions/PermissionIndex.vue
+++ resources/js/Pages/Admin/Permissions/PermissionIndex.vue
@@ -36,7 +36,10 @@ defineProps(["permissions"]);
               <TableRow v-for="permission in permissions" :key="permission.id" class="border-t">
                 <TableDataCell>{{ permission.id }}</TableDataCell>
                 <TableDataCell>{{ permission.name }}</TableDataCell>
-                <TableDataCell>Edit/Delete</TableDataCell>
+                <TableDataCell>
+                  <Link :href="route('permissions.edit', permission.id)" class="text-green-400 hover:text-green-600">Edit</Link> /
+                   <Link :href="route('permissions.destroy', permission.id)" method="delete" as="button" class="text-red-400 hover:text-red-600">Delete</Link>
+                </TableDataCell>
               </TableRow>
             </template>
           </Table>
